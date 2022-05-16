a05c440cdd53d60ec680d599fbaf4b7f753618cb
4Qian <li.qian3@northeastern.edu>
2022-05-15 17:52:09 -0700

    Updates

---
+++ precache-manifest.780e3f97db79485992fafcd5cc2c1474.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c10fde1643bbac27c314",
+    "url": "/qianliwebsite/static/css/main.866bf10e.chunk.css"
+  },
+  {
+    "revision": "c10fde1643bbac27c314",
+    "url": "/qianliwebsite/static/js/main.c10fde16.chunk.js"
+  },
+  {
+    "revision": "517527aa6758397229f0",
+    "url": "/qianliwebsite/static/js/1.517527aa.chunk.js"
+  },
+  {
+    "revision": "09b13c4de0d7e7682020",
+    "url": "/qianliwebsite/static/js/2.09b13c4d.chunk.js"
+  },
+  {
+    "revision": "6594894714ab4d4e964a",
+    "url": "/qianliwebsite/static/js/runtime~main.65948947.js"
+  },
+  {
+    "revision": "ddf47aa8d35bce6bb458bafdca5bd18a",
+    "url": "/qianliwebsite/index.html"
+  }
+];
